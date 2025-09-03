a9146bdcd67ed070ecc6182629c063c30bd6923c
obrunorf <obrunorf@gmail.com>
2025-09-03 13:15:10 -0400

    commit0

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import *
+
+def main():
+    nodulo = TextNode("Galo Doido", TextType.LINK, 'https://www.boot.dev')
+    print(nodulo)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN_TEXT = "text (plain)"
+    BOLT_TEXT = "**Bold text**"
+    ITALIC_TEXT ="_Italic text_"
+    CODE_TEXT = '`Code text`'
+    LINK = "Links, in this format: [anchor text](url)"
+    IMAGE= "Images, in this format: ![alt text](url)"
+
+    
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__(self, __value) -> bool:
+        try:
+            if self.text == __value.text and self.text_type == __value.text_type and self.url == __value.url:
+                return True
+            return False
+        except:
+            return False
+    
+    def __repr__(self) -> str:
+        return f'TextNode({self.text}, {self.text_type.value}, {self.url})'
